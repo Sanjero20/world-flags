7127977a70c2dab5470a3febe8c05c9a6df5dc75
Sanjero <gelosantos72@gmail.com>
2023-01-09 18:21:38 +0800

    Add axios to fetch data from REST COUNTRIES API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+type countryData = {
+  name: string;
+  capital: string;
+  region: string;
+  flag: string;
+  independent: boolean;
+};
+
 function App() {
+  const [countries, setCountries] = useState();
+
+  // Fetch the DATAs
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v2/all?fields=region,name,flag,capital')
+      .then((res) => {
+        const responseData = res.data;
+        const countries = responseData.map(
+          ({ independent, ...data }: countryData) => data
+        );
+
+        setCountries(countries);
+      })
+      .catch((err) => {
+        console.clear();
+        console.log(err);
+      });
+  }, []);
+
   return <div>World flags</div>;
 }
 
