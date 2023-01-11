e0a75b8b5224c4fc64be6c17de314c76279af9bc
Sanjero <gelosantos72@gmail.com>
2023-01-11 12:19:26 +0800

    Display an image during empty state

--- src/components/Flags.tsx
+++ src/components/Flags.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { Country, CountryProps } from './types/country.types';
 import Card from './Card';
 
+import world from '../assets/world.svg';
+
 // Render the list of countries
 function Flags({ list }: CountryProps) {
+  if (list.length <= 0) {
+    return (
+      <div className="search-error">
+        <img className="world" src={world} alt="" />
+        <p>THIS COUNTRY DOES NOT EXIST</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <div className="flags">
